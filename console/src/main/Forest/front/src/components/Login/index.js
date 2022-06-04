b3ed4af26a1ada47f510a054d99b030194e47c1a
lana2402 <l_saakovi@cu.edu.ge>
2022-06-04 21:08:59 +0400

    add cors

--- console/src/main/Forest/front/src/components/Login/index.js
+++ console/src/main/Forest/front/src/components/Login/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from "../Nav";
 import Footer from "../Footer";
 import {
@@ -19,8 +19,11 @@ import {
 import {DescrBtn, DescrBtnLink} from "../MainPage/MainPageElements";
 import {FooterBg, FooterIcon, FooterIcons} from "../Footer/FooterElemet";
 import {FacebookIcon, InstagramIcon, LinkedinIcon, WhatsappIcon} from "../svg";
+import {Axios as axios} from "axios";
 
 const LoginElements = () => {
+    const [userinfo, setUserInfo] = useState([]);
+
     return (
         <>
             <Nav/>
@@ -57,10 +60,23 @@ const LoginElements = () => {
                             <label><input type="radio" value="Benefactor" name="name"/>Benefactor</label>
                         </InputElements>
                         <Registr>
-                            <input type="text" placeholder="Enter Email" name="email" />
+                            <input id = "userEmal" type="text" placeholder="Enter Email" name="email" />
                                 <input type="password" placeholder="Enter Password" name="psw" />
                                     <input type="password" placeholder="Repeat Password" name="psw-repeat" />
                                             <Registerbtn>Register</Registerbtn>
+                                            <button onClick={()=>{
+                                                fetch("http://localhost:8080/customer-service-api/findall")
+                                                    .then((res) => res.json())
+                                                    .then((json) => {
+                                                        setUserInfo(json);
+                                                        console.log(json);
+                                                        console.log(userinfo);
+                                                        console.log("sdfsdfsdf");
+                                                        console.log(json[0]);
+                                                        console.log(json[0].email);
+                                                        document.getElementById("userEmal").value = json[0].email;
+                                                    })
+                                            }}>TEST</button>
                         </Registr>
                     </PageCreate>
                 </Regform>
